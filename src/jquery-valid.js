00111df1373db473159ca88464aeb08c8870568b
Madzia <magda2609@gmail.com>
2015-05-18 06:31:48 +0200

    file reader

--- src/jquery-valid.js
+++ src/jquery-valid.js
@@ -6,7 +6,6 @@
       var value = $(that).val();
       var err = false;
       var errType = {};
-      // //console.log($(that).val());
 
       if(value.length === 0){
         errType.empty = true;
@@ -15,7 +14,6 @@
       if(options.size){
         if(options.size.min){
           if(value.length < options.size.min){
-            // //console.log("size.min err");
             err = true;
             if(!errType.size){ errType.size = {}; }
             errType.size.min = true;
@@ -23,7 +21,6 @@
         }
         if(options.size.max){
           if(value.length > options.size.max){
-            // //console.log("size.max err");
             err = true;
             if(!errType.size){ errType.size = {}; }
             errType.size.max = true;
@@ -37,10 +34,7 @@
         } else{
           patt = new RegExp(options.regexp.pat);
         }
-        // //console.log(patt);
-        // //console.log(patt.test(value));
         if(!patt.test(value)){
-          // //console.log("regexp err");
           err = true;
           errType.regexp = true;
         }
@@ -51,14 +45,9 @@
           errType.condition = true;
         }
       }
-      //console.log(err);
-      
-      // console.log(errType);
-
       return { "valid": !err, "errType": errType };
     },
     "postcode": function (that, options) {
-      // console.log(options);
       var settings = $.extend(
         {
           "size": 
@@ -78,7 +67,6 @@
     },
     "fields": function (that) {
       var err = false;
-      //console.log(that);
 
       $(that.field).each(function () { 
         var isValid = valid[ that.type ](this, that.options);
@@ -98,30 +86,23 @@
   }
 
   var methods = {
-    'init': function ( options ) {
-      console.log("Usage: $(selector).valid(type, options)");
-      console.log("Go to http://github.com/mmotel/jquery-plugin-validation/ for more details.");
-
-      return this;
-    },
     'field': function ( method, options, callback, trigger ) {
-      //console.log(options);
 
       return this.each(function () {
         var doValid = function () { 
           var isValid = valid[ method ](this, options);
-
           if(isValid.valid){
             callback.call(this);
-	    $("label:last").html("dsa")
+	    $("label:last").html("");
       	    $("#submitBtn").removeAttr("disabled");
-	    $( "#label:last" ).load( "/files/kody.csv", { limit: 25 }, function() {
-		  alert( "The last 25 entries in the feed have been loaded" );
-	    });
+		d3.text("file:///home/magda/biu/biu_jQuery/rozw/examples/fields/files/kody.csv", function(data) {
+			var dane = data.split(/[\n\r]/).map(function(line) { return line.split(';')})
+			for (var i=0; i<dane.length; i++)
+				if (dane[i][0] == ($("#postcodeInput").val())) $( "label:last" ).html(dane[i][2]);
+		});
           } 
           else {
             callback.call(this, isValid.errType);
-	    $("label:last").html("")
       	    $("#submitBtn").attr("disabled", "disabled");
           }
        }
@@ -142,15 +123,10 @@
     if( valid[ method ] && method !== "fields" ) {
       return methods.field.apply( this, arguments );
     }
-    else if( methods[ method ] && method !== "field" ){
-      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
-    }
-    else if ( typeof method === 'object' || ! method ) {
-      return methods.init.apply( this, arguments );
-    }
     else {
       $.error('Method ' + method + ' does not exists in jQuery.valid');
     }
   };
 
 })( jQuery );
+
